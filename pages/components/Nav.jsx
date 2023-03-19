c357b7636533034e304b0c23c797121f7893a5e1
Cezary Draus <108001877+Taaxuus@users.noreply.github.com>
2023-03-19 20:43:10 +0100

    second commit

--- pages/components/Nav.jsx
+++ pages/components/Nav.jsx
@@ -1,30 +1,54 @@
 import Link from "next/link";
 
-function Navigation() {
+const Navigation = () => {
+	// const handleNav = () => {
+	// 	nav.classList.toggle("nav--active");
+
+	// 	navBtnBars.classList.remove("black-bars-color");
+
+	// 	allNavItems.forEach((item) => {
+	// 		item.addEventListener("click", () => {
+	// 			nav.classList.remove("nav--active");
+	// 		});
+	// 	});
+
+	// 	handleNavItemsAnimation();
+	// };
+
+	// const handleNavItemsAnimation = () => {
+	// 	let delayTime = 0;
+
+	// 	allNavItems.forEach((item) => {
+	// 		item.classList.toggle("nav-items-animation");
+	// 		item.style.animationDelay = "." + delayTime + "s";
+	// 		delayTime++;
+	// 	});
+	// };
+
 	return (
 		<nav className="nav">
 			<div className="nav__items">
-				<Link href="/">
-					<a className="nav__item">Strona Główna</a>
+				<Link href="/" className="nav__item">
+					Strona Główna
 				</Link>
-				<Link href="#actu">
-					<a className="nav__item">Aktualności</a>
+				<Link href="#actu" className="nav__item">
+					Aktualności
 				</Link>
-				<Link href="#about">
-					<a className="nav__item">O projekcie</a>
+				<Link href="#about" className="nav__item">
+					O projekcie
 				</Link>
-				<Link href="#eco">
-					<a className="nav__item">Ekosystemy</a>
+				<Link href="#eco" className="nav__item">
+					Ekosystemy
 				</Link>
-				<Link href="#contact">
-					<a className="nav__item">Kontakt</a>
+				<Link href="#contact" className="nav__item">
+					Kontakt
 				</Link>
-				<Link href="./publiorders.html">
-					<a className="nav__item">Zamówienia</a>
+				<Link href="./publiorders.html" className="nav__item">
+					Zamówienia
 				</Link>
 			</div>
 		</nav>
 	);
-}
+};
 
 export default Navigation;
